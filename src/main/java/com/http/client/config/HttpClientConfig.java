19df34f607c785157e084b5ad0d9af9278819917
linzhou <linzhou@duiba.com.cn>
2021-12-13 11:49:50 +0800

    自动装配

--- src/main/java/com/http/client/config/HttpClientConfig.java
+++ src/main/java/com/http/client/config/HttpClientConfig.java
@@ -4,6 +4,7 @@ package com.http.client.config;
 import lombok.Data;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -17,6 +18,7 @@ import java.util.List;
  */
 @Component
 @ConfigurationProperties(prefix = "httpclient")
+@ComponentScan("com.http.client")
 @Data
 public class HttpClientConfig {
 
